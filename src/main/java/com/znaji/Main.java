016a6f2e09cbd4504f0e82c2387b3e6fa98fe02d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-26 21:30:24 +0100

    #Spring core: use parent - we can define a template bean (we can defin it with abstarct so we don't have an instace of it created, we can even ommit the class) - on the beans that need to use it add parent="idOfParent", need to have compatible properties(not necessarly a subtype of parent), also we can extend or even override attributes from the parent.

--- src/main/java/com/znaji/Main.java
+++ src/main/java/com/znaji/Main.java
@@ -2,15 +2,39 @@ package com.znaji;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
-        final AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        final ClassPathXmlApplicationContext xmlApplicationContext
+                = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        context.registerShutdownHook();
+        final DataSource userDataSource = xmlApplicationContext.getBean("userDataSource", DataSource.class);
+        try (Connection connection = userDataSource.getConnection()) {
+            connection.createStatement().execute("CREATE TABLE users (id INT, name VARCHAR(50))");
+            connection.createStatement().execute("INSERT INTO users (id, name) VALUES (1, 'John Doe')");
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users");
+            while (resultSet.next()) {
+                System.out.println("User id: " + resultSet.getInt("id") + ", name: " + resultSet.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        final LifeCycleBean lifeCycleBean = context.getBean(LifeCycleBean.class);
-        lifeCycleBean.print();
+        final DataSource productDataSource = xmlApplicationContext.getBean("productDataSource", DataSource.class);
+        try (Connection connection = productDataSource.getConnection()) {
+            connection.createStatement().execute("CREATE TABLE products (id INT, name VARCHAR(50))");
+            connection.createStatement().execute("INSERT INTO products (id, name) VALUES (1, 'Book')");
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM products");
+            while (resultSet.next()) {
+                System.out.println("Product id: " + resultSet.getInt("id") + ", name: " + resultSet.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
